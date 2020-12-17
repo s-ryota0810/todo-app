0e802fbe52a14eff1b2868c3674b953c3e35a1c7
ryota.s.0810 <akaashi0810@gmail.com>
2020-12-17 22:59:00 +0900

    new, createアクションの追加/flash,errorメッセージの追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -4,4 +4,25 @@ class BoardsController < ApplicationController
   def index
     @boards = Board.all
   end
+  
+  def new
+    @board = current_user.boards.build
+  end
+  
+  def create
+    @board = current_user.boards.build(board_params)
+    if @board.save
+      redirect_to boards_path, notice: '保存しました'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new
+    end
+  end
+  
+  
+  private
+  
+  def board_params
+    params.require(:board).permit(:user_id, :name, :description)
+  end
 end
