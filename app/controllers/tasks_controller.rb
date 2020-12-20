d7226845732eddb32a5979fb3e48393e5b594c2c
ryota.s.0810 <akaashi0810@gmail.com>
2020-12-20 18:48:42 +0900

    createアクションの追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,5 +6,21 @@ class TasksController < ApplicationController
   end
   
   def create
+    board = Board.find(params[:board_id])
+    @task = board.tasks.build(task_params)
+    if @task.save
+      redirect_to board_path(@task.board_id), notice: 'タスクを追加しました'
+    else
+      flash.now[:error] = 'タスクの追加に失敗しました'
+      render :new
+    end
   end
+  
+  
+  private
+  
+    def task_params
+      params.require(:task).permit(:title, :content, :limit)
+    end
+  
 end
