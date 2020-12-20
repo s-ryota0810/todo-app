963f6b2296cf7a2c5c7f476b8c9a48cd82306b5d
ryota.s.0810 <akaashi0810@gmail.com>
2020-12-20 17:51:18 +0900

    Taskモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_17_115120) do
+ActiveRecord::Schema.define(version: 2020_12_20_084218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2020_12_17_115120) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_boards_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.bigint "board_id", null: false
+    t.string "title", null: false
+    t.text "content", null: false
+    t.datetime "limit"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["board_id"], name: "index_tasks_on_board_id"
   end
 
   create_table "users", force: :cascade do |t|
