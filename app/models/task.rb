64f342a625a13b47a8170b2d3f73c161c66ccfe5
ryota.s.0810 <akaashi0810@gmail.com>
2020-12-21 23:00:16 +0900

    Commentモデルの追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -19,6 +19,7 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   belongs_to :board
+  has_many :comments, dependent: :destroy
   
   has_one_attached :eyecatch
   
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :boards, dependent: :destroy
+  has_many :comments, dependent: :destroy
   
   
   
