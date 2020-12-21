f85715162a18d18f276121fbb59cfdb6e7c0d493
ryota.s.0810 <akaashi0810@gmail.com>
2020-12-21 22:47:53 +0900

    annotateの導入

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :bigint           not null, primary key
+#  content    :text             not null
+#  limit      :datetime
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  board_id   :bigint           not null
+#
+# Indexes
+#
+#  index_tasks_on_board_id  (board_id)
+#
 class Task < ApplicationRecord
   
   validates :title, presence: true
